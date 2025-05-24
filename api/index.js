6a521ec0951cdd74be663652c8c14347ed4f1531
Alan-Botoshansky <alanboto2002@gmail.com>
2025-05-24 11:13:15 -0300

    BE: Added endpoint to create users
    Co-authored-by: Ivanboto <Ivanboto@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,11 +1,37 @@
 const express = require('express')
+const { PrismaClient } = require('./generated/prisma')
+const bycrypt = require('bcrypt')
+
+const PORT = 3000
+const SALT_ROUNDS = 10
+
 const app = express()
-const port = 3000
+app.use(express.json())
+
+const prisma = new PrismaClient()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/users', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required' })
+  }
+
+  const hashedPassword = await bycrypt.hash(password, SALT_ROUNDS)
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  })
+  res.status(201).json({ id: user.id })
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`)
+  console.log(`Press Ctrl+C to quit.`)
 })
