f8d9b1faf64f1e3318d3a83b8c94ac64bbb940d3
Ivanboto <ivanboto2002@gmail.com>
2025-05-26 11:41:56 -0300

    BE: Added endpoint to log in
    Co-Authored-By: Alan-Botoshansky <78547391+Alan-Botoshansky@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { PrismaClient } = require('./generated/prisma')
-const bycrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 
 const PORT = 3000
 const SALT_ROUNDS = 10
@@ -21,7 +21,7 @@ app.post('/users', async (req, res) => {
     return res.status(400).json({ error: 'Email and password are required' })
   }
 
-  const hashedPassword = await bycrypt.hash(password, SALT_ROUNDS)
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
   const user = await prisma.user.create({
     data: {
       email,
@@ -29,6 +29,29 @@ app.post('/users', async (req, res) => {
     },
   })
   res.status(201).json({ id: user.id })
+})
+
+app.post('/auth/login', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (!user) {
+    return res.status(401).json({ error: 'User does not exist' })
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password)
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: 'Invalid password' })
+  }
+
+  res.status(200).json({ message: 'Login successful' })
 })
 
 app.listen(PORT, () => {
